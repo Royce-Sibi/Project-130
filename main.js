60bad8c42c0cb127d253136f4d6e5a664b9bb950
Royce Sibichan <92983553+Royce-Sibi@users.noreply.github.com>
2022-10-17 17:59:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,95 @@
+song= "";
+song2= "";
+song_status = "";
+song2_status = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
+function preload()
+{
+song = loadSound("music.mp3");
+song2 = loadSound("music2.mp3");
+}
+
+function setup()
+{
+canvas = createCanvas(600, 500);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = "+ scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY);
+    }
+}
+
+function draw()
+{
+image(video, 0, 0, 600, 500);
+
+song_status = song.isPlaying();
+song2_status = song2.isPlaying();
+
+fill('#FFC0CB');
+stroke('#FFC0CB');
+
+if(scoreRightWrist > 0.2)
+{
+    circle(rightWristX,rightWristY,20);
+
+    song2.stop();
+
+    if(song_status == false)
+    {
+        song.play();
+        document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song";
+    }
+}
+if(scoreLeftWrist > 0.2)
+{
+circle(leftWristX, leftWristY,20);
+
+song.stop();
+
+if(song2_status == false)
+{
+    song2.play();
+    document.getElementById("song").innerHTML = "Playing Peter Pan Song";
+}
+}
+}
+
+function play()
+{
+song.play();
+song.setVolume(1);
+song.rate(1);
+}
